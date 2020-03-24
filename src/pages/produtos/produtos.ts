d85347902e481403d5e38fe5d5ab247f3286bcfb
Fabio Souza <fabmacsouza@gmail.com>
2020-03-24 17:30:24 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -9,23 +10,34 @@ import { ProdutoService } from '../../services/domain/produto.service';
   selector: 'page-produtos',
   templateUrl: 'produtos.html',
 })
-export class ProdutosPage {
+  export class ProdutosPage {
 
-  items : ProdutoDTO[];
+    items : ProdutoDTO[];
 
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public produtoService: ProdutoService) {
-  }
+    constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      public produtoService: ProdutoService) {
+    }
 
-  ionViewDidLoad() {
-    let categoria_id = this.navParams.get('categoria_id');
-    this.produtoService.findByCategoria(categoria_id)
-      .subscribe(response => {
-        this.items = response['content'];
+    ionViewDidLoad() {
+      let categoria_id = this.navParams.get('categoria_id');
+      this.produtoService.findByCategoria(categoria_id)
+        .subscribe(response => {
+          this.items = response['content'];
+          this.loadImageUrls();
+        },
+        error => {});
+    }
 
-  },
-  error => {});
-}
+    loadImageUrls() {
+      for (var i=0; i<this.items.length; i++) {
+        let item = this.items[i];
+        this.produtoService.getSmallImageFromBucket(item.id)
+          .subscribe(response => {
+            item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+          },
+          error => {});
+      }
+    }  
 }
